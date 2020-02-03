fab38976ec2caaed0eabf07028be2b7e94744938
Sid Ravalia <sid.ravalia@herschel.com>
2020-02-03 15:29:26 -0800

    Creation of SchedulerComponents
    Created scheduler services inside bluecherry space. This service is not being updated to use BC specific Dlls as there will only be one VisualCron instance service across diffirent spaces.
    
    Resolves: #HSC-514

---
+++ Utility/SchedulerComponents/PushSvcEndPointDefinition/PushSvc.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Diagnostics;
+
+namespace PushSvcEndPointDefinition
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PushSvc" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PushSvc.svc or PushSvc.svc.cs at the Solution Explorer and start debugging.
+    public class PushSvc : IPushSvc
+    {
+        public void InitializeProcess(string guid, string queueName, decimal maxRate, bool isBatch, int maxBatchSize)
+        {
+            //Start process
+            Trace.WriteLine("We are now in the InitializeProcess method of the Push Service.  GUID: " + guid);
+        }
+    }
+}
---
+++ Utility/SchedulerComponents/TestClient/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Corp.Integration.Utility;
+
+namespace TestClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSingletonOnRamp_Click(object sender, EventArgs e)
+        {
+            Tasks tasks = new Tasks();
+
+            tasks.OnRampSingletonInitializer(Guid.NewGuid().ToString(), "http://hip-development-loadbalancer-1-1047608554.us-west-2.elb.amazonaws.com/AppServices/BaozunAppService/SalesDocOnRampSvc.v1/ProcessBaozunBatchFiles.svc");
+
+
+        }
+    }
+}
