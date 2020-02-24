49d7a808ee8ca87a114e5d0989171e40f1798e99
jeanpouabou <52759152+jeanpouabou@users.noreply.github.com>
2020-02-24 15:18:42 -0800

    Adding BlueCherryException class.

---
+++ Utility/BC_API_Calls/BC_API_Calls/BCException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BC.Integration.APICalls
+{
+
+
+    public class BlueCherryException : Exception
+    {
+        public BlueCherryException()
+        {
+            //adding comment
+        }
+
+        public BlueCherryException(string message)
+            : base(message)
+        {
+        }
+
+        public BlueCherryException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
